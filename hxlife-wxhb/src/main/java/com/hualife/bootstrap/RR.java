14621ffcf473e68914ce2fb69bd2a54d7adf3dad
wupeixu <wupeixu@lexue.com>
2019-01-04 13:57:45 +0800

    2

--- hxlife-wxhb/src/main/java/com/hualife/bootstrap/RR.java
+++ hxlife-wxhb/src/main/java/com/hualife/bootstrap/RR.java
@@ -28,9 +28,9 @@ public class RR {
 //		Constant.OSS_PDF_PATH +
 		String dateStr = DateFormatUtil.formatDate("yyyy/MM/dd/", da);
 //		String ossPath =  "/" + "jand" + "/" + "1" + "/"+dateStr+".pdf";
-		String ossPath = dateStr+"111"+".p1FF11";
+		String ossPath = dateStr+"111"+".p1";
 		String oss;
-		String ossPat = dateStr+"111"+".p1FFF11";
+		String ossPat = dateStr+"111"+".p1";
 		System.out.println(ossPath);
 	}
 
